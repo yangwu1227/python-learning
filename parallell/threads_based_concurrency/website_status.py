d5a5d575a96b4b74cbf99947c0ba947cc92814b3
YangWu1227 <yangwu2020@gmail.com>
2022-11-12 23:03:02 -0600

    update: complete threadpoolexecutor

---
+++ parallell/threads_based_concurrency/website_status.py
@@ -0,0 +1,69 @@
+from urllib.request import urlopen
+from urllib.error import URLError
+from urllib.error import HTTPError
+from http import HTTPStatus
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
+
+# ------------------------ Get the status of a website ----------------------- #
+
+
+def get_website_status(url):
+    # Handle connection errors
+    try:
+        # Open a connection to the server with a timeout
+        with urlopen(url, timeout=3) as connection:
+            # Get the response code, e.g. 200
+            code = connection.getcode()
+            return code
+    except HTTPError as e:  # Raised when HTTP error occurs
+        return e.code
+    except URLError as e:  # URLError could be raised if we are unable to connect
+        return e.reason
+    except Exception as e:  # Catch all other exceptions
+        return e
+
+# --------------- Interpret an HTTP response code into a status -------------- #
+
+
+def get_status(code):
+    if code == HTTPStatus.OK:
+        return 'OK'
+    return 'ERROR'
+
+# -------------------- Check status of a list of websites -------------------- #
+
+
+def check_status_urls(urls):
+    # Create the thread pool with as many workers as there are URLs
+    with ThreadPoolExecutor(len(urls)) as executor:
+        # Issue each task, map of futures to urls
+        future_to_url = {executor.submit(
+            get_website_status, url): url for url in urls}  # Each key is a future object for a given, which is its value
+        # Get results as they are available, since we do not care about order or return values
+        for future in as_completed(future_to_url):
+            # Get the url for the future
+            url = future_to_url[future]
+            # Get the status for the website
+            code = future.result()
+            # Interpret the status
+            status = get_status(code)
+            # Report status
+            print(f'{url:20s}\t{status:5s}\t{code}')
+
+
+# Protect the entry point
+if __name__ == '__main__':
+    # list of urls to check
+    urls = ['https://twitter.com',
+            'https://google.com',
+            'https://facebook.com',
+            'https://reddit.com',
+            'https://youtube.com',
+            'https://amazon.com',
+            'https://wikipedia.org',
+            'https://ebay.com',
+            'https://instagram.com',
+            'https://cnn.com']
+    # check all urls
+    check_status_urls(urls)
